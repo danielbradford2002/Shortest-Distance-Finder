afafe5a31b03c101fb60c8c1b51814bcea30ffa3
donkeynice <80865361+donkeynice@users.noreply.github.com>
2022-12-27 14:08:44 -0500

    Update GraphTest.java

--- GraphTest.java
+++ GraphTest.java
@@ -1,11 +1,3 @@
-// --== CS400 File Header Information ==--
-// Name: Daniel Bradford
-// Email: dcbradford@wisc.edu
-// Team: AG
-// TA: Yuye
-// Lecturer: Gary Dahl
-// Notes to Grader: <optional extra notes>
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
